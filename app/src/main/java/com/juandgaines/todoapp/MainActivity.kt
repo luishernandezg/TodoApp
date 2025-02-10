1ea6a1957daa835ecda30ea000da7987d734d501
Luis Hernandez <luis.guaicaipuro.h.a@gmail.com>
2025-02-10 17:14:51 -0300

    Compose: Fases y Recomposición - class 06/19

--- app/src/main/java/com/juandgaines/todoapp/MainActivity.kt
+++ app/src/main/java/com/juandgaines/todoapp/MainActivity.kt
@@ -4,24 +4,60 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import com.juandgaines.todoapp.ui.components.HelloWorld
+import androidx.compose.ui.unit.dp
 import com.juandgaines.todoapp.ui.theme.TodoAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
-            TodoAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    HelloWorld(modifier = Modifier.padding(innerPadding))
-                }
+            TodoAppTheme() {
+                HelloWorldView()
             }
         }
     }
+}
+
+@Composable
+fun HelloWorldView() {
+    var isShown by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier.padding(56.dp)
+    ) {
+        MessageText(isShown)
+        HelloWorldText {
+            isShown = !isShown
+        }
+    }
+}
+
+@Composable
+fun MessageText(isShown: Boolean) {
+    if (isShown) {
+        Text("This is a message")
+    }
+}
+
+@Composable
+fun HelloWorldText(onClick: () -> Unit) {
+    Text(
+        "Hello, world!",
+        modifier = Modifier.clickable {
+            onClick()
+        }
+    )
 }
 
